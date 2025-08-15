5e7a7c911d09abc264e8d4af18dec477f6ae90ef
aleksej <natash79@inbox.ru>
2025-08-15 11:46:47 +0300

    !

--- PP_bac/settings.py
+++ PP_bac/settings.py
@@ -6,6 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
+#1
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['pp-bao.ru', 'api.pp-bao.ru', 'localhost', '127.0.0.1']
