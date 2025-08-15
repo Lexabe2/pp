a181c18073ad09987592a4d89c8dcda25e9a7685
aleksej <natash79@inbox.ru>
2025-08-15 12:07:31 +0300

    Тест

--- PP_bac/settings.py
+++ PP_bac/settings.py
@@ -5,7 +5,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-# 11
 DEBUG = True
 
 ALLOWED_HOSTS = ['pp-bao.ru', 'api.pp-bao.ru', 'localhost', '127.0.0.1']
